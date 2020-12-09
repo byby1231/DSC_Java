32805271ef7687e95255c85d0ce6dbc86597982c
byby1231 <byby1231@yahoo.com.tw>
2020-12-09 22:50:07 +0800

    JAVA第一次作業完成
    JAVA第一次作業完成

---
+++ Work/day01/Hello.java
@@ -0,0 +1,15 @@
+class Hellowww{
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+		System.out.println();//
+		System.out.println("Hello World!");
+		}
+}
+
+class Hello222{
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+		System.out.println();//
+		System.out.println("Hello World!");
+		}
+}
